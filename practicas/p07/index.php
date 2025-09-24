90ea0b239d3c9b41c6c4bf8f8e83441291c0c9df
Celeste <cobix.1789@gmail.com>
2025-09-24 11:45:54 -0600

    Se agregó el primer ejercicio

---
+++ practicas/p07/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 7</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        require_once 'src/funciones.php';
+        if(isset($_GET['num']))
+        {
+            $num = $_GET['num'];
+            multiplode5y7($num);
+        }
+    ?>
+
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p07/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    <?php
+        if(isset($_POST["name"]) && isset($_POST["email"]))
+        {
+            echo $_POST["name"];
+            echo '<br>';
+            echo $_POST["email"];
+        }
+    ?>
+</body>
+</html>
