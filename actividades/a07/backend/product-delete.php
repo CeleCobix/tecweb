6866c54c463ccdab30d92d643a97d9ce6e08303b
Celeste <cobix.1789@gmail.com>
2025-11-10 21:46:24 -0600

    Se modificaron los demás archivos del backend

--- actividades/a07/backend/product-delete.php
+++ actividades/a07/backend/product-delete.php
@@ -1,5 +1,5 @@
 <?php
-    include_once __DIR__.'/database.php';
+    /*include_once __DIR__.'/database.php';
 
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array(
@@ -21,5 +21,14 @@
     } 
     
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+    echo json_encode($data, JSON_PRETTY_PRINT);*/
+
+    use MYAPI\PRODUCTS;
+    require_once __DIR__.'/myapi/Products.php';
+
+    if (isset($_POST['id'])) {
+        $products = new Products('marketzone');
+        $products->delete($_POST['id']);
+        echo $products->getData();
+    }
 ?>
--- actividades/a07/backend/product-edit.php
+++ actividades/a07/backend/product-edit.php
@@ -1,5 +1,5 @@
 <?php
-    include_once __DIR__.'/database.php';
+    /*include_once __DIR__.'/database.php';
 
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array(
@@ -24,5 +24,16 @@
     } 
     
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+    echo json_encode($data, JSON_PRETTY_PRINT);*/
+
+    use MYAPI\PRODUCTS; // 1. Usar el namespace correcto [cite: 48]
+    require_once __DIR__.'/myapi/Products.php'; // 2. Incluir la clase Products [cite: 48]
+
+    if (isset($_POST['id'])) {
+        $jsonOBJ = json_decode(json_encode($_POST));
+
+        $products = new Products('marketzone'); // 3. Crear instancia [cite: 49]
+        $products->edit($jsonOBJ); // 4. Usar el método correcto [cite: 50]
+        echo $products->getData(); // 5. Devolver la respuesta en JSON [cite: 51]
+    }
 ?>
