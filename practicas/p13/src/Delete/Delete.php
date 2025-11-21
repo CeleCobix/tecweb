7e9781c4d09b53e7e16a192b2d10a005b23cc8f0
Celeste <cobix.1789@gmail.com>
2025-11-21 17:14:17 -0600

    Ajustes según el diagrama UML

---
+++ practicas/p13/src/Delete/Delete.php
@@ -0,0 +1,37 @@
+<?php
+    namespace TECWEB\MYAPI\Delete;
+
+    use TECWEB\MYAPI\DataBase;
+
+    class ProductDelete extends DataBase {
+        private $data;
+
+        public function __construct($db, $user='root', $pass='yisshanli') {
+            $this->data = array();
+            parent::__construct($db, $user, $pass);
+        }
+
+        public function delete($id) {
+            $this->data = array(
+                'status'  => 'error',
+                'message' => 'La consulta falló'
+            );
+
+            if(isset($id)) {
+                $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+                
+                if ($this->conexion->query($sql)) {
+                    $this->data['status'] =  "success";
+                    $this->data['message'] =  "Producto eliminado";
+                } else {
+                    $this->data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($this->conexion);
+                }
+                $this->conexion->close();
+            }
+        }
+
+        public function getData() {
+            return json_encode($this->data, JSON_PRETTY_PRINT);
+        }
+    }
+?>
